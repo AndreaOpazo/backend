a7249a83bfbb8d02fcccdd9196033e616f847cd0
AndreaOpazo <andree.2593@gmail.com>
2021-09-07 23:58:11 -0300

    en proceso

---
+++ src/clase-13/data.ts
@@ -0,0 +1,14 @@
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  thumbnail: string;
+}
+const data: Product[] = [];
+
+export default data;
+
+export type Message = {
+  emailUser: string;
+  message: string;
+}
